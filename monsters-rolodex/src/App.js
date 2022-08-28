8c4ee99cdd95e3a71a4141a1b2092b6e2346996a
bipingosain <bipincodes@outlook.com>
2022-08-28 16:52:34 +0530

    class component and fetch api with component did mount

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,20 +1,29 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      monsters: [{ name: "Linda" }, { name: "Frank" }, { name: "Jacky" }],
-    };
+    this.state = { monsters: [] };
   }
 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        this.setState({ monsters: data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster, index) => (
-          <h1 key={index}>{monster.name}</h1>
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
         ))}
       </div>
     );
